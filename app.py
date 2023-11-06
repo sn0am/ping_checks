48444834dc9c9fdab3da58b55c185157343d8093
sn0am <81282692+sn0am@users.noreply.github.com>
2023-11-05 21:10:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import time
 
 ping_url = os.environ.get("ping_url")
 ping_interval = int(os.environ.get("ping_interval"))
+ping_timeout = int(os.environ.get("ping_timeout"))
 
 while True:
     try:
         print('Attempting to ping...')
-        requests.get(f'{ping_url}', timeout=10)
+        requests.get(f'{ping_url}', timeout=ping_timeout)
         print('Ping was successful!')
     except requests.RequestException as e:
         print("Ping failed: %s" % e)
